1ba087f9fb0e5d7ce49f23579886bdcf6d94ad43
AmmarEldik <vektorea60@gmail.com>
2022-10-18 12:47:02 +0300

    completed asignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1* num2;
 
+
+
+let random = Math.floor(1.5);
+
+
+let max = Math.max(1.2, 4 ,20);
+
+let num3 = 10 ;
+let num4 = 6 ;
+let mod = num3 % num4 ;
 // Write your code here
